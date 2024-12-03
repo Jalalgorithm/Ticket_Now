3ef6134bf31a238719ed459092b69afda04c02ee
Jalalgorithm <temitomzi@gmail.com>
2024-12-03 01:49:30 +0100

    Added category to event

--- Event_Ticket/api/models.py
+++ Event_Ticket/api/models.py
@@ -3,6 +3,14 @@ from django.db import models
 # Create your models here.
 
 
+class EventCategory(models.Model):
+    name= models.CharField(max_length=100 , unique=True)
+    description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.name
+    
+
 class Event(models.Model):
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,17 +18,12 @@ class Event(models.Model):
     location=models.CharField(max_length=255)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+    category= models.ForeignKey(EventCategory , on_delete=models.CASCADE , related_name='events')
     
     def __str__(self):
         return self.title
     
-class EventCategory(models.Model):
-    name= models.CharField(max_length=255)
-    description = models.TextField()
-    
-    def __str__(self):
-        return self.name
-    
+
     
     
     
